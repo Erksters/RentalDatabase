94a52eac525fe49e9bbcf0ae1c041cf3377ff9d9
Erick PC <erksters@gmail.com>
2020-10-16 23:34:55 -0500

    Added Sample Property Owner Get Request Filter

---
+++ GETS/GetPropertyOwner.sql
@@ -0,0 +1,17 @@
+
+Declare @isFirstName NVARCHAR = Null;
+Declare @isLastName NVARCHAR = Null;
+Declare @isPhone Int = Null;
+Declare @isEmail NVARCHAR = Null;
+Declare @isID Int = Null
+
+--Try define the declarations from get request
+--Catch keep it as null the query will know
+
+Select * 
+From #PropertyOwners PO
+Where (@isID IS NOT NULL AND (PO.PropertyOwnerID = @isID)) OR
+(@isFirstName IS NOT NULL AND (PO.FirstName = @isFirstName)) OR
+(@isLastName IS NOT NULL AND (PO.LastName = @isLastName)) OR
+(@isEmail IS NOT NULL AND (PO.Email = @isEmail)) OR
+(@isPhone IS NOT NULL AND (PO.Phone = @isPhone) OR
