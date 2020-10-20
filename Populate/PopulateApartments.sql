74226e30d0f6fd602d851bc8bf998b507bfeb690
Erick PC <erksters@gmail.com>
2020-10-19 20:52:54 -0500

    Added population script

---
+++ Populate/PopulateApartments.sql
@@ -0,0 +1,103 @@
+--populate Table PropertyOwnder
+--Author Erick Saenz
+
+INSERT INTO Apartment(
+    MonthlyPrice,
+	Street1,
+	Street2,
+	City,
+	ZipCode,
+	Available,
+	DateAvailable,
+	YearBuilt,
+	PetsAllowed,
+	NumberRooms,
+	NumberBath,
+	Refurbished,
+	CentralAC,
+	MyPropertyOwner
+)
+VALUES 
+			(
+			599,            --Monthly Price 
+			'123 Main',
+			null,
+			'Manhattan',
+			'66502',
+			1,              --Available
+			GetDate(),	    --Date Available
+			'2020-01-01',	--Year Built
+			1,				--Pets Allowed
+			3,				--NumberRooms
+			2,				--Number Baths
+			0,				--Refurbished
+			1,				--Central AC
+			1               --MyPropertyOwner
+		  ),
+		  (
+			499,            --Monthly Price
+			'323 Main',
+			'#2',
+			'Topeka',
+			'66506',
+			1,             --Available
+			GetDate(),     --Date Available
+			'2020-01-01',  --Year Built
+			1,             --Pets Allowed
+			3,             --NumberRooms
+			2,             --Number Baths
+			0,			   --Refurbished
+			1,			   --Central AC
+			1			   --MyPropertyOwner
+		  ),
+		  (
+			399,        --Monthly Price
+			'579 Main',
+			'#18',
+			'Topeka',
+			'66506',
+			0,            --Available
+			GetDate(),     --Date Available
+			'2019-01-01',  --Year Built
+			0,             --Pets Allowed
+			1,             --NumberRooms
+			1,             --Number Baths
+			1,			   --Refurbished
+			0,			   --Central AC
+			3			   --MyPropertyOwner
+		  ),
+		  (
+			799,        --Monthly Price
+			'889 Main',
+			null,
+			'Kansas City',
+			'66509',
+			1,             --Available
+			GetDate(),     --Date Available
+			'2014-01-01',  --Year Built
+			0,             --Pets Allowed
+			4,             --NumberRooms
+			2,             --Number Baths
+			0,			   --Refurbished
+			1,			   --Central AC
+			4			   --MyPropertyOwner
+		  ),
+		  (
+			699,        --Monthly Price
+			'777 Main',
+			'#9',
+			'Dodge City',
+			'66501',
+			0,             --Available
+			GetDate(),     --Date Available
+			'2018-01-01',  --Year Built
+			1,             --Pets Allowed
+			3,             --NumberRooms
+			1,             --Number Baths
+			0,			   --Refurbished
+			1,			   --Central AC
+			5			   --MyPropertyOwner
+		  )
+		  
+		  Select * 
+		  From Apartment
